5266e2891040849a1e3984ddd6de9cbe5043c2f6
Soorya U <sooryau7@gmail.com>
2024-07-11 22:46:14 +0530

    fix: Seperated Library Functions
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- context/value-converter.js
+++
@@ -1,9 +0,0 @@
-import { nativeToScVal } from "@stellar/stellar-sdk";
-
-export const stringToScValString = (value) => {
-  return nativeToScVal(value);
-};
-
-export const numberToU64 = (value) => {
-  return nativeToScVal(value, { type: "u64" });
-};
