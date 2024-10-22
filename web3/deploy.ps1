7d09b017922bc6736d314101ac86922f14f9cb0a
Soorya U <sooryau7@gmail.com>
2024-10-22 23:44:46 +0530

    chore: Added Positional Arguments to Powershell Scripts
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- web3/deploy.ps1
+++ web3/deploy.ps1
@@ -1,26 +1,29 @@
-$source = "bored-man"
-$network = "testnet"
+param(
+  [string]$Source,
+  [string]$Network
+)
+
 $build_path = "target/wasm32-unknown-unknown/release/democrachain.wasm"
 
-$owner_address = stellar keys address $source
+$owner_address = stellar keys address $Source
 
 Write-Host "Building Contract..."
 
 stellar contract build > $null 2>&1
 
-Write-Host "Deploying to $network..."
+Write-Host "Deploying to $Network..."
 
 $contract_address = stellar contract deploy `
   --wasm $build_path `
-  --source $source `
-  --network $network
+  --source $Source `
+  --network $Network
 
 Write-Host "Invoking Initialization Function..."
 
 stellar contract invoke `
   --id $contract_address `
-  --source $source `
-  --network $network `
+  --source $Source `
+  --network $Network `
   -- init `
   --owner_address $owner_address  `
   > $null 2>&1
