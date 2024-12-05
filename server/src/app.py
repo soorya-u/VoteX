f856cc5b29d1f29213c7ef04ff6c636694feaca6
Soorya U <sooryau7@gmail.com>
2024-12-05 21:52:08 +0530

    chore: Few Tunings in Server
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- server/src/app.py
+++ server/src/app.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 from .routes import api_route
 
 app = FastAPI()
+
+frontend_url = os.getenv("FRONTEND_URL") or ""
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[frontend_url],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(HTTPException)
--- server/src/routes/face.py
+++ server/src/routes/face.py
@@ -1,15 +1,20 @@
 from fastapi import APIRouter, UploadFile, status
+from typing import Literal
 
 from ..controllers import face_verification_handler, face_registration_handler
 
 face_route = APIRouter(prefix="/face")
 
+USER_TYPE = Literal["candidate", "voter"]
+
 
 @face_route.post("/register/{public_key}", status_code=status.HTTP_201_CREATED)
-async def face_registration_route(file: UploadFile, public_key: str):
-    return await face_registration_handler(file, public_key)
+async def face_registration_route(file: UploadFile, public_key: str,
+                                  user_type: USER_TYPE):
+    return await face_registration_handler(file, public_key, user_type)
 
 
 @face_route.post("/compare/{public_key}", status_code=status.HTTP_200_OK)
-async def face_comparision_route(file: UploadFile, public_key: str):
-    return await face_verification_handler(file, public_key)
+async def face_comparision_route(file: UploadFile, public_key: str,
+                                 user_type: USER_TYPE):
+    return await face_verification_handler(file, public_key, user_type)
