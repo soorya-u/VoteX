a58ba9a0f64c11f8620b17edfd6fc4fa1338fd56
Soorya U <100910996+soorya-u@users.noreply.github.com>
2024-12-06 21:57:18 +0530

    chore: Updated Contract Variables and Function Names in Frontend and Backend (#56)
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/constants/contract.ts
+++ client/src/constants/contract.ts
@@ -1,41 +1,34 @@
 export const networkPassphrase = "Test SDF Network ; September 2015";
 
-export const RPC_URL = "https://soroban-testnet.stellar.org"
+export const RPC_URL = "https://soroban-testnet.stellar.org";
 
-export enum ContractFunctions {
-  ChangeOwner = "change_owner",
+export enum AdminContractFunctions {
+  ChangeAdmin = "change_admin",
   ResetContract = "reset_contract",
-
+  SetVoterAsApproved = "set_voter_as_approved",
+  SetCandidateAsVerified = "set_candidate_as_verified",
   SetVotingPeriod = "set_voting_period",
-
-  RegisterVoter = "register_voter",
-  UpdateVoter = "update_voter",
-  ApproveVoter = "approve_voter",
-  RejectVoter = "reject_voter",
-
-  RegisterCandidate = "register_candidate",
-  UpdateCandidate = "update_candidate",
   ApproveCandidate = "approve_candidate",
   RejectCandidate = "reject_candidate",
+}
 
-  GetAllVotersWhoVoted = "get_all_voters_who_voted",
-
+export enum UserContractFunctions {
+  RegisterVoter = "register_voter",
+  UpdateVoter = "update_voter",
+  RegisterCandidate = "register_candidate",
+  UpdateCandidate = "update_candidate",
   GiveVote = "vote",
-  GetCurrentVotingStatus = "get_current_voting_status",
-  GetWinningCandidate = "get_winning_candidate",
 }
 
 export enum ContractVariables {
-  Owner = "Owner",
+  Admin = "Admin",
   StartTime = "StartTime",
   EndTime = "EndTime",
   RegisteredVoters = "RegVot",
+  RegisteredCandidates = "RegCan",
+  ApprovedVoters = "ApproVot",
+  ApprovedCandidates = "ApproCan",
+  VotedVoters = "VotVoted",
   Voter = "Voter",
   Candidate = "Candidate",
-  RegisteredCandidates = "RegCan",
-  ApprovedCandidates = "ApproCan",
-  ApprovedVoters = "ApproVot",
-  VotedVoters = "VotVoted",
-  TotalVoters = "votIdCntr",
-  TotalCandidates = "canIdCntr",
 }
