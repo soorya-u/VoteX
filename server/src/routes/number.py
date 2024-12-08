a404b54ea84c8d5560d39633e525f5d75e8cb60c
Soorya U <sooryau7@gmail.com>
2024-12-09 00:53:06 +0530

    chore: Server side Changes
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- server/src/routes/number.py
+++ server/src/routes/number.py
@@ -6,6 +6,7 @@ from ..controllers import number_identification_handler, number_verification_han
 number_route = APIRouter(prefix="/number")
 
 Candidate = Literal["candidate"]
+Voter = Literal["voter"]
 
 
 @number_route.post("/identify/{public_key}",
@@ -17,5 +18,6 @@ async def number_identification_route(file: UploadFile, public_key: str):
 @number_route.get("/verify/{public_key}")
 async def number_verification_route(otp: str,
                                     public_key: str,
-                                    user_type: Optional[Candidate] = None):
+                                    user_type: Optional[Candidate
+                                                        | Voter] = None):
     return await number_verification_handler(otp, public_key, user_type)
